99f55c5c71f6db59f769056069019543a4679ebf
jiangqing <819657451@qq.com>
2019-09-10 23:44:00 +0800

    配置中心

---
+++ configclient/src/main/java/com/jq/configclient/ConfigclientApplication.java
@@ -0,0 +1,26 @@
+package com.jq.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigclientApplication.class, args);
+	}
+
+
+	@Value("${hello}")
+	private String hello;
+
+	@RequestMapping("/hello")
+	public String from() {
+		return this.hello;
+	}
+}
